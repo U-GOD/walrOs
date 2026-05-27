c33b06c4875b95bc279d09591590d697b538ba31
UGOD <kabufredrick9@gmail.com>
2026-05-27 01:27:11 +0000

    config: add Sui and Walrus network constants

---
+++ frontend/src/lib/constants.ts
@@ -0,0 +1,3 @@
+export const PACKAGE_ID = "0x9aa252a966f4baa6021716fffa7797a308746636927095c98a679d280ca30d6d";
+export const SUI_RPC_URL = "https://fullnode.testnet.sui.io:443";
+export const WALRUS_AGGREGATOR_URL = "https://aggregator.walrus-testnet.walrus.space";
